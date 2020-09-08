2a5ba0c610019cde9bc3dfb47e5c37a5a7647370
Taurus Canis <acdole09@gmail.com>
2020-09-08 11:19:52 -0400

    added slash to root url

--- ace_it_test_prep/urls.py
+++ ace_it_test_prep/urls.py
@@ -6,7 +6,7 @@ print("URLS")
 
 app_name = 'ace_it_test_prep'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('/', views.index, name='index'),
     path('signup', views.signup, name='signup'),
     path('login_user', views.login_user, name='login_user'),
     path('logout_user', views.logout_user, name='logout_user'),
