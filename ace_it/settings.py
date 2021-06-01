b58ec9c01e509e734c63245a8ce505fa00cb13ae
Taurus Canis <acdole09@gmail.com>
2021-06-01 17:15:23 -0400

    revised static settings in settings.py

--- ace_it/settings.py
+++ ace_it/settings.py
@@ -144,8 +144,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = os.path.join(BASE_DIR, 'ace_it_test_prep/static/')
-# STATIC_URL = '/static/'
+# STATIC_URL = os.path.join(BASE_DIR, 'ace_it_test_prep/static/')
+# # STATIC_URL = '/static/'
+#
+# # Activate Django-Heroku.
+# django_heroku.settings(locals())
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
